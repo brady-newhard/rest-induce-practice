b79ef4bca05c07373902dc50bdc735872d27dd6f
Brady Newhard <brady.russell@gmail.com>
2024-12-03 22:00:14 -0500

    EDIT route change

---
+++ data/books.js
@@ -0,0 +1,8 @@
+module.exports = [
+    { id: 1, title: 'The Great Gatsby', author: 'F. Scott Fitzgerald' },
+    { id: 2, title: '1984', author: 'George Orwell' },
+    { id: 3, title: 'To Kill a Mockingbird', author: 'Harper Lee' }
+];
+
+// exports.books = books
+
---
+++ server.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const morgan = require('morgan');
+const methodOverride = require("method-override");
+const app = express();
+const books = require("./data/books");
+const path = require("path")
+
+//-------------MIDDLEWARE-----------------//
+
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(methodOverride("_method"))
+app.set('view engine', 'ejs');
+app.set('views', './views'); 
+app.use(express.static(path.join(__dirname, 'public')))
+
+//-----ROUTE w/ I.N.D.U.C.E.S-------//
+
+// Index
+app.get("/", (req, res) => {
+    res.render("home")
+})
+
+// Index
+app.get("/books", (req, res) => {
+    res.render("books", {title: "Book List", books })
+})
+
+// New
+app.get('/books/new', (req, res) => {
+    // create a new item
+    res.render('books/new', { title: 'New Book' })
+});
+
+// POST
+app.post('/books', (req, res) => {
+    const newBook = {
+        id: books.length + 1,
+        title: req.body.title || "new book",
+        author: req.body.author || "new author"
+    }
+    books.push(newBook);
+    res.status(201).redirect('/books')
+})
+
+// SHOW
+app.get('/books/:id', (req, res) => {
+    const book = books.find(book => book.id === parseInt(req.params.id));
+    if (book) {
+        res.render('books/show', { title: 'Book Details', book })
+    } else {
+        res.status(404).render('404/notFound', { title: "Book not found" })
+    }
+})
+
+// EDIT
+app.put('/books/:id', (req, res) => {
+    const bookId = parseInt(req.params.id);
+    const bookIndex = books.findIndex(book => book.id === bookId)
+    if (bookIndex !== -1) {
+        books[bookIndex] = { ...books[bookIndex], ...req.body }
+        res.status(200).redirect(`/books`)
+    } else {
+        res.status(404).render('404/notFound', { title: 'Book Not Found' })
+    }
+})
+
+// DELETE
+app.delete("/books/:id", (req, res) => {
+    const bookId = parseInt(req.params.id);
+
+})
+//---------------------Listener-------------------------//
+app.listen(3000, () => {
+    console.log('🎧Listening on http://localhost:3000')
+})
