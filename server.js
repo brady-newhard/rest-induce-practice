3903ff372ce14725f522c27a718c482166128f63
Brady Newhard <brady.russell@gmail.com>
2024-12-08 22:53:53 -0500

    Book Route

--- server.js
+++ server.js
@@ -1,22 +1,13 @@
-const dotenv = require('dotenv');
-dotenv.config();
+require('dotenv').config()
 const express = require('express');
 const morgan = require('morgan');
-const methodOverride = require("method-override");
+const methodOverride = require('method-override')
 const app = express();
-const books = require("./data/books");
-const path = require("path")
-const mongoose = require('mongoose');
-const Book = require("./models/book");
+const path = require('path');
 
 //-------------CONFIGURE MONGOOSE-----------------//
 
-mongoose.connect(process.env.MONGODB_URI, {
-}).then(() => {
-    console.log('Connected to MongoDB');
-}).catch((err) => {
-    console.log(err);
-})
+require('./configs/database');
 
 //-------------MIDDLEWARE-----------------//
 
@@ -36,70 +27,8 @@ app.use("/", require("./routes/seed"))
 // Home Route
 app.use('/', require('./routes/home'));
 
-// Index
-app.get("/books", (req, res) => {
-    res.render("books", {title: "Book List", books })
-})
-
-// New
-app.get('/books/new', (req, res) => {
-    // create a new item
-    res.render('books/new', { title: 'New Book' })
-});
-
-// POST/CREATE
-app.post('/books', (req, res) => {
-    const newBook = {
-        id: books.length + 1,
-        title: req.body.title || "new book",
-        author: req.body.author || "new author"
-    }
-    books.push(newBook);
-    res.status(201).redirect('/books')
-})
-
-// SHOW
-app.get('/books/:id', (req, res) => {
-    const book = books.find(book => book.id === parseInt(req.params.id));
-    if (book) {
-        res.render('books/show', { title: 'Book Details', book })
-    } else {
-        res.status(404).render('404/notFound', { title: "Book not found" })
-    }
-})
-
-app.get('/books/:id/edit', (req, res) => {
-    const book = books.find(book => book.id === parseInt(req.params.id));
-    if (book) {
-        res.render('books/edit', { title: 'Edit Book', book });
-    } else {
-        res.status(404).render('404/notFound', { title: 'Book Not Found!' })
-    }
-})
-
-// EDIT
-app.put('/books/:id', (req, res) => {
-    const bookId = parseInt(req.params.id);
-    const bookIndex = books.findIndex(book => book.id === bookId)
-    if (bookIndex !== -1) {
-        books[bookIndex] = { ...books[bookIndex], ...req.body }
-        res.status(200).redirect(`/books`)
-    } else {
-        res.status(404).render('404/notFound', { title: 'Book Not Found' })
-    }
-})
-
-// DELETE
-app.delete('/books/:id', (req, res) => {
-    const bookId = parseInt(req.params.id);
-    const bookIndex = books.findIndex(book => book.id === bookId);
-    if (bookIndex !== -1) {
-        books.splice(bookIndex, 1);
-    } else {
-        res.status(404).render('404/notFound', { title: 'Book Not Found' });
-    }
-    res.redirect('/books');
-});
+// Book Routes
+app.use('/', require('./routes/book'));
 
 //---------------------Listener-------------------------//
 app.listen(process.env.PORT, () => {
