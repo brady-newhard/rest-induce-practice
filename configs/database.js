4696418567159c5634c3120ee0aa06a062b28393
Brady Newhard <brady.russell@gmail.com>
2024-12-08 23:19:33 -0500

    mongodb to mongo

--- configs/database.js
+++ configs/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const db = mongoose.connection;
 
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGO_URI);
 
 db.on("connected", () => console.log("mongoDB connected"))
 db.on("error", (err) => console.log(err.message, "is mongoDB connected"));
