f14d958469c311ba831218ff0935d7b1bf7365d4
Brady Newhard <brady.russell@gmail.com>
2024-12-08 22:07:36 -0500

    feat:add home and seed routes

---
+++ controllers/home.js
@@ -0,0 +1,6 @@
+function index(req, res) {
+    res.render('home', { title: 'Home Page' })
+}
+
+
+module.exports = { index }
---
+++ controllers/seed.js
@@ -0,0 +1,13 @@
+const Book = require('../models/book');
+const books = require('../data/books')
+
+async function seedData(req, res) {
+    try {
+        await Book.insertMany(books);
+        res.status(201).send('Books seeded successfully')
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Error seeding books')
+    }
+}
+module.exports = { seedData }
